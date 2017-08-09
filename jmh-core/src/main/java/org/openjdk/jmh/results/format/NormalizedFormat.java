2816cc690f53ec9187842138a03eabce3dc0f7ca
Aaron Hoffer <aaron.hoffer@connexta.com>
2017-08-09 07:31:34 -0700

    Fixed missing rows

--- jmh-core/src/main/java/org/openjdk/jmh/results/format/NormalizedFormat.java
+++ jmh-core/src/main/java/org/openjdk/jmh/results/format/NormalizedFormat.java
@@ -29,15 +29,13 @@ import java.io.PrintStream;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
-import org.openjdk.jmh.results.AggregationPolicy;
 import org.openjdk.jmh.results.Result;
 import org.openjdk.jmh.results.ResultRole;
 import org.openjdk.jmh.results.RunResult;
@@ -56,145 +54,233 @@ public class NormalizedFormat implements ResultFormat {
 
     @Override
     public void writeOut(Collection<RunResult> results) {
-        Set<String> parameterNames = getParameterNames(results);
 
         List<List<String>> table = new ArrayList<>();
+
+        Set<String> allSecondaryMetricNames = results.stream()
+                .map(x -> x.getSecondaryResults()
+                        .keySet())
+                .flatMap(Set::stream)
+                .map(y -> Outcome.trimPunctuation(y))
+                .collect(Collectors.toSet());
+        Collection<String> parameterNames = null;
         for (RunResult run : results) {
 
-            List<Result> actualResults = new ArrayList<>();
-            actualResults.add(run.getPrimaryResult());
-            actualResults.addAll(run.getSecondaryResults()
-                    .entrySet()
-                    .stream()
-                    .map(Map.Entry::getValue)
-                    .collect(Collectors.toList()));
+            // All metric names found
+            Set<String> allMetrrics = new HashSet<>();
 
-            for (Result actualResult : actualResults) {
-                List<String> row = new ArrayList<>();
+            //Primary results
+            Outcome outcome = Outcome.from(run, run.getPrimaryResult());
+            allMetrrics.add(outcome.getMetricName());
+            table.add(outcome.asRow());
 
-                // Current results has information about outcomes, but not about test parameters
-                Result currentResult = actualResult;
+            // Any RunResult will provide the parameter names.
+            // Because all RunResults should have the parameters.
+            parameterNames = outcome.getParameterNames();
 
-                // Test Name
-                row.add(run.getPrimaryResult()
-                        .getLabel());
+            //Secondary Results
+            Set<String> writtenSecondaryMetrics = new HashSet<>();
+            run.getSecondaryResults()
+                    .forEach((metricName, result) -> {
+                        Outcome o = (Outcome.from(run, result));
+                        table.add(o.asRow());
+                        writtenSecondaryMetrics.add(o.getMetricName());
+                    });
 
-                // Parameter Values
-                for (String pName : parameterNames) {
-                    String paramValue = run.getParams()
-                            .getParam(pName);
-                    row.add(paramValue);
-                }
-
-                // Run "mode" (single shot, throughput, ...)
-                row.add(run.getParams()
-                        .getMode()
-                        .shortLabel());
-
-                // Metric Name
-                String metricName;
-                if (currentResult.getRole()
-                        .equals(ResultRole.PRIMARY)) {
-                    metricName = run.getParams()
-                            .getMode()
-                            .longLabel();
-                } else {
-                    metricName = trimPunctuation(currentResult.getLabel());
-                }
-                row.add(metricName);
-
-                // Number of iterations used for measurement
-                row.add(String.valueOf(currentResult.getSampleCount()));
-
-                // Aggregation policy (sum, min, max, ...)
-                row.add(getPolicyOf(currentResult).toString());
-
-                // Value of the metric
-                row.add(String.valueOf(currentResult.getScore()));
-
-                // Margin of error
-                row.add(String.valueOf(doubleToString(currentResult.getScoreError())));
-
-                // Units of the metric
-                row.add(currentResult.getScoreUnit());
-
-                // Print the row
-                table.add(row);
-            }
+            //Missing results
+            HashSet<String> missingMetrics = new HashSet<>(allSecondaryMetricNames);
+            missingMetrics.removeAll(writtenSecondaryMetrics);
+            missingMetrics.forEach(metric -> {
+                Outcome o = MissingOutcome.from(run, metric);
+                table.add(o.asRow());
+            });
         }
 
-        try {
-            List<String> header = new ArrayList<>();
-            header.add("Test");
-            header.addAll(parameterNames);
-            header.add("Benchmark Mode");
-            header.add("Metric");
-            header.add("Sample Size");
-            header.add("Statistic Type");
-            header.add("Statistic Value");
-            header.add("Statistical Margin of Error");
-            header.add("Units");
+        List<String> header = new ArrayList<>();
+        header.add("Test");
+        header.addAll(parameterNames);
+        header.add("Metric");
+        header.add("Sample Size");
+        header.add("Statistic Type");
+        header.add("Statistic Value");
+        header.add("Statistical Margin of Error");
+        header.add("Units");
 
+        try {
             printer.printRecord(header);
             printer.printRecords(table);
+
+            table.forEach(r -> System.err.println(r.stream()
+                    .collect(Collectors.joining(", "))));
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
 
-    private AggregationPolicy getPolicyOf(Result currentResult) {
-        return (AggregationPolicy) getFieldContents(currentResult, "policy");
-    }
+    public static class Outcome {
 
-    Set<String> getParameterNames(Collection<RunResult> results) {
-        // Use sorted set for parameter values line up with the parameter name in the header.
-        return results.stream()
-                .map(x -> x.getParams()
-                        .getParamsKeys())
-                .flatMap(Collection::stream)
-                .collect(Collectors.toCollection(TreeSet::new));
-    }
+        protected RunResult run;
 
-    public String trimPunctuation(String input) {
-        String goodChars = "^[^a-zA-Z]+";
-        return input.replaceFirst(goodChars, "")
-                .replaceAll(goodChars, "");
+        protected Result result;
 
-    }
-
-    // NaN messes up with I want to do with my spreadsheet.
-    // Use zero in place of NaN.
-    String doubleToString(Double value) {
-        return value.isNaN() ? "0" : String.valueOf(value);
-    }
-
-    Object getFieldContents(Object object, String fieldName) {
-        Class myClass = object.getClass();
-        Field myField = null;
-        try {
-            myField = getField(myClass, fieldName);
-            myField.setAccessible(true); //required if field is not normally accessible
-            return myField.get(object);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            return null;
+        private Outcome(RunResult run, Result result) {
+            this.run = run;
+            this.result = result;
         }
-    }
 
-    Field getField(Class clazz, String fieldName) throws NoSuchFieldException {
-        try {
-            return clazz.getDeclaredField(fieldName);
-        } catch (NoSuchFieldException e) {
-            Class superClass = clazz.getSuperclass();
-            if (superClass == null) {
-                throw e;
-            } else {
-                return getField(superClass, fieldName);
+        public static Outcome from(RunResult run, Result result) {
+            return new Outcome(run, result);
+        }
+
+        public static String trimPunctuation(String input) {
+            String goodChars = "^[^a-zA-Z]+";
+            return input.replaceFirst(goodChars, "")
+                    .replaceAll(goodChars, "");
+        }
+
+        public String getMarginOfError() {
+            return doubleToString(result.getScoreError());
+        }
+
+        public String getMetricName() {
+            return isPrimary() ?
+                    run.getParams()
+                            .getMode()
+                            .longLabel() :
+                    trimPunctuation(result.getLabel());
+        }
+
+        Collection<String> getParameterNames() {
+            return run.getParams()
+                    .getParamsKeys();
+        }
+
+        List<String> getParameterValues(Collection<String> parameterNames) {
+            return parameterNames.stream()
+                    .map(p -> run.getParams()
+                            .getParam(p))
+                    .collect(Collectors.toList());
+        }
+
+        public ResultRole getRole() {
+            return result.getRole();
+        }
+
+        public String getSampleSize() {
+            return String.valueOf(result.getSampleCount());
+        }
+
+        String getStatisticName() {
+            // How the measurements from multiple iterations are summarized
+            // as a single number.
+            return getFieldContents(result, "policy").toString();
+        }
+
+        public String getStatisticValue() {
+            return String.valueOf(result.getScore());
+        }
+
+        public String getTestName() {
+            return run.getPrimaryResult()
+                    .getLabel();
+        }
+
+        public String getUnits() {
+            return result.getScoreUnit();
+        }
+
+        public boolean isPrimary() {
+            return getRole().equals(ResultRole.PRIMARY);
+        }
+
+        public List<String> asRow() {
+            List<String> row = new ArrayList<>();
+            row.add(getTestName());
+            row.addAll(getParameterValues(getParameterNames()));
+            row.add(getMetricName());
+            row.add(getSampleSize());
+            row.add(getStatisticName());
+            row.add(getStatisticValue());
+            row.add(getMarginOfError());
+            row.add(getUnits());
+            return row;
+        }
+
+        String doubleToString(Double value) {
+            return value.isNaN() ? "NA" : String.valueOf(value);
+        }
+
+        Object getFieldContents(Object object, String fieldName) {
+            Class myClass = object.getClass();
+            Field myField;
+            try {
+                myField = getField(myClass, fieldName);
+                myField.setAccessible(true); //required if field is not normally accessible
+                return myField.get(object);
+            } catch (NoSuchFieldException | IllegalAccessException e) {
+                return null;
+            }
+        }
+
+        Field getField(Class clazz, String fieldName) throws NoSuchFieldException {
+            try {
+                return clazz.getDeclaredField(fieldName);
+            } catch (NoSuchFieldException e) {
+                Class superClass = clazz.getSuperclass();
+                if (superClass == null) {
+                    throw e;
+                } else {
+                    return getField(superClass, fieldName);
+                }
             }
         }
     }
+
+    public static class MissingOutcome extends Outcome {
+        protected String metricName;
+
+        private MissingOutcome(RunResult run, String missingMetric) {
+            super(run, null);
+            metricName = missingMetric;
+        }
+
+        public static Outcome from(RunResult run, String missingMetric) {
+            return new MissingOutcome(run, missingMetric);
+        }
+
+        public String getMarginOfError() {
+            return "NA";
+        }
+
+        public String getMetricName() {
+            return metricName;
+        }
+
+        @Override
+        public ResultRole getRole() {
+            return ResultRole.OMITTED;
+        }
+
+        String getStatisticName() {
+            return "NONE";
+        }
+
+        public String getSampleSize() {
+            return "0";
+        }
+
+        public String getStatisticValue() {
+            return "0";
+        }
+
+        public String getUnits() {
+            return "NONE";
+        }
+    }
 }
+
+
 
 
 
